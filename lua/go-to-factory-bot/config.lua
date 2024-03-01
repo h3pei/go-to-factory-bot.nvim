fca7597506593d34b619424d596b96c119068910
mogulla3 <hs.iarn3@gmail.com>
2024-03-02 00:38:02 +0900

    Add stylua

--- lua/go-to-factory-bot/config.lua
+++ lua/go-to-factory-bot/config.lua
@@ -13,10 +13,10 @@ function Config.setup(user_config)
   config = vim.tbl_deep_extend("force", default_config, user_config)
 
   vim.validate({
-    custom_patterns = { config.custom_patterns, 'table' },
-    definition_file_path = { config.definition_file_path, 'string' },
-    jump_command = { config.jump_command, 'string' },
-    suffix = { config.suffix, 'string' },
+    custom_patterns = { config.custom_patterns, "table" },
+    definition_file_path = { config.definition_file_path, "string" },
+    jump_command = { config.jump_command, "string" },
+    suffix = { config.suffix, "string" },
   })
 end
 
