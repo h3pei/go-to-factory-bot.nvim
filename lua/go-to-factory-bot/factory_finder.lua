bdfaf8cd9c1a66449eba3b4fdb0063d504e8ca44
mogulla3 <hs.iarn3@gmail.com>
2024-03-02 18:03:12 +0900

    Rename argument name of find_by_name

--- lua/go-to-factory-bot/factory_finder.lua
+++ lua/go-to-factory-bot/factory_finder.lua
@@ -3,13 +3,13 @@ local Inflector = require("go-to-factory-bot.inflector")
 local M = {}
 
 ---@param factory_name string
----@param factory_path string
+---@param definition_file_path string
 ---@param suffix string
 ---@return string|nil
-function M.find_by_name(factory_name, factory_path, suffix)
+function M.find_by_name(factory_name, definition_file_path, suffix)
   local plural_name = Inflector.pluralize(factory_name)
   local factory_file_name = plural_name .. suffix .. ".rb"
-  local paths = vim.fs.find(factory_file_name, { path = factory_path, type = "file", limit = 1 })
+  local paths = vim.fs.find(factory_file_name, { path = definition_file_path, type = "file", limit = 1 })
 
   if not vim.tbl_isempty(paths) then
     return paths[1]
